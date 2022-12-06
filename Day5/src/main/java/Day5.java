c0f0176003b53acb818806fc5daf377f977c2731
Erik Nyquist <erik.nyquist@uc.se>
2022-12-06 08:23:09 +0100

    Day 5 part 2

--- Day5/src/main/java/Day5.java
+++ Day5/src/main/java/Day5.java
@@ -76,5 +76,42 @@ public class Day5 {
 
 
     private static void execise2(List<String> lines) {
+        var config = lines.stream().takeWhile(l->l.length()>0).map(l -> l.split("(?<=\\G.{4})")).collect(Collectors.toUnmodifiableList());
+        var numberOfColumns = config.get(config.size()-1).length;
+        var initialColumns = IntStream.range(0, numberOfColumns).mapToObj(i -> {
+            return IntStream.range(0, config.size()-1).mapToObj(j -> {
+                if (config.get(j).length > i) {
+                    var box = config.get(j)[i];
+                    return box.startsWith(" ") ? null : box;
+                } else return null;
+            }).filter(Objects::nonNull).collect(Collectors.toList());
+        }).collect(Collectors.toList());
+        System.out.println(numberOfColumns);
+        var moves = lines.subList(config.size()+1,lines.size()).stream().map(l -> {
+            var matcher = movePattern.matcher(l);
+            if (matcher.matches()) {
+                return new Action(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)), Integer.parseInt(matcher.group(3)));
+            } else {
+                throw new RuntimeException("Regexp error");
+            }
+        }).collect(Collectors.toList());
+        var iterator = moves.iterator();
+        var columns = initialColumns;
+        while (iterator.hasNext()) {
+            var m = iterator.next();
+            var newConfig = new ArrayList<List<String>>(columns.size());
+            for(int i = 0; i<columns.size(); i++) {
+                if (i == m.fromColumn-1) {
+                    newConfig.add(columns.get(i).subList(m.moveCount, columns.get(i).size()));
+                } else if (i == m.toColumn-1) {
+                    var added = new ArrayList<>(columns.get(m.fromColumn-1).subList(0, m.moveCount));
+                    newConfig.add(Stream.concat(added.stream(), columns.get(i).stream()).collect(Collectors.toList()));
+                } else {
+                    newConfig.add(columns.get(i));
+                }
+            }
+            columns = newConfig;
+        };
+        System.out.println(columns.stream().map(c -> c.get(0).substring(1,2)).collect(Collectors.joining()));
     }
 }
